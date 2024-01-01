0df5e0fc45a44015833176f90ca3802f8bb9b2a3
amx100 <amx281@gmail.com>
2024-01-01 15:55:09 +0100

    moded route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DrugController;
+use App\Http\Controllers\PurchaseHistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('customers', CustomerController::class);
+Route::resource('drugs', DrugController::class);
+Route::resource('purchasehistories', PurchaseHistoryController::class);
+
+
+
+Route::get('/purchasehistories/{id}', 'PurchaseHistoryController@show');
