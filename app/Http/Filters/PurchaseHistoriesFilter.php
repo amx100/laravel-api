1fc695612dc60b01fb6c6a1413b548c889fd7ba8
amx100 <amx281@gmail.com>
2024-01-01 15:53:35 +0100

    Added Filters

---
+++ app/Http/Filters/PurchaseHistoriesFilter.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Http\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+use App\Http\Filters\ApiFilter;
+
+class PurchaseHistoriesFilter extends ApiFilter
+{
+    protected $safeParams = [
+        'customerId' => ['eq'],
+        'drugId' => ['eq'],
+        'quantity' => ['eq', 'gt', 'lt'],
+        'purchaseDate' => ['eq', 'gt', 'lt'],
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>='
+    ];
+
+    public function apply(Builder $builder, $filterData)
+    {
+        foreach ($filterData as $key => $value) {
+            if (array_key_exists($key, $this->safeParams)) {
+                $operator = $this->operatorMap[$this->safeParams[$key][0]];
+                $builder->where($key, $operator, $value);
+            }
+        }
+
+        return $builder;
+    }
+}
